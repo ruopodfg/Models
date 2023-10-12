56808ea96110d677c0d63c9a55bf8fd617946662
Ivan Kratenko <121116219+ruopodfg@users.noreply.github.com>
2023-10-12 12:24:35 +0300

    Add files via upload

---
+++ 12345/Gamingmarketplace/Payments/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Payment(models.Model):
+    username = models.CharField(max_length=255)
+    amount_money = models.IntegerField()
+    comment = models.CharField(max_length=255, blank=True, null= True)
